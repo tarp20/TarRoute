be77c5694b613cea91904c0d7994fad2e352eb0c
Taras Piont <taras.piont@gmail.com>
2021-01-29 19:25:32 +0100

    final heroku c

--- TarRoute/settings/production.py
+++ TarRoute/settings/production.py
@@ -41,7 +41,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['tarroute.herokuapp.com']
 
 
 # Application definition
